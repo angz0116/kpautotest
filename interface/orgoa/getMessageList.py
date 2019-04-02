6dd9ce1f1f686df2ff5d3b28bf2a1d7f7d302fc2
zhaoai <za_ai@163.com>
2019-04-02 16:51:24 +0800

    2019-04-02，提交用户认证信息

---
+++ interface/orgoa/getMessageList.py
@@ -0,0 +1,89 @@
+# -*- coding:utf-8 -*-
+
+from utils.baseLog import MyLog
+from utils.baseHttp import ConfigHttp
+from utils.baseUtils import *
+import unittest
+import paramunittest
+import datetime
+interfaceNo = "getMessageList"
+name = "通知列表"
+
+req = ConfigHttp()
+
+
+@paramunittest.parametrized(*get_xls("interfaces.xls", interfaceNo))
+class 通知列表(unittest.TestCase):
+    def setParameters(self, No, 测试结果, 请求报文, 返回报文, 测试用例, url, 预期结果):
+        self.No = str(No)
+        self.url = str(url)
+
+    def setUp(self):
+        self.log = MyLog.get_log()
+        self.logger = self.log.logger
+        self.log.build_start_line(interfaceNo + name + "CASE " + self.No)
+        print(interfaceNo + name + "CASE " + self.No)
+
+    """通知列表"""
+    def test_body(self):
+        req.httpname = "KPTEST"
+        # 获取执行接口的url
+        self.url = get_excel("url", self.No, interfaceNo)
+        # 获取登录sheet页中token
+        self.token = get_excel("token", self.No, "login")
+
+        self.data = {
+            "v": "3.11.0",
+            "system": "5",
+            "device_model": "HUAWEI P10",
+            "system_version": "V1.0.0",
+            "channel": "5"
+        }
+        print(self.data)
+        if self.token=="":
+            self.urlq = self.url
+            self.logger.info(interfaceNo+">>>>token为空====="+self.urlq)
+        else:
+            self.urlq = self.url+"&&token="+self.token
+            self.logger.info(interfaceNo + ">>>>token====="+self.urlq)
+        req.set_url(self.urlq)
+        req.set_data(self.data)
+        self.response = req.post()
+        print(self.response)
+        try:
+            self.retcode = self.response["code"]
+        except Exception:
+            self.logger.error("报文返回为空！")
+            print("报文返回为空！")
+        self.check_result()
+        self.wr_excel()
+
+    # 检查数据结果
+    def check_result(self):
+        try:
+            self.assertEqual(self.retcode, 0, self.logger.info("是否获取通知列表"))
+            set_excel("pass", "测试结果", self.No, interfaceNo)
+            self.logger.info("测试通过")
+        except AssertionError:
+            set_excel("fail", "测试结果", self.No, interfaceNo)
+            self.msg = self.response["msg"]
+            self.logger.error("测试失败")
+        self.msg = self.response["msg"]
+        self.logger.info(self.msg)
+
+    # 写入xls文件中
+    def wr_excel(self):
+        set_excel(self.data, "请求报文", self.No, interfaceNo)
+        set_excel(self.response, "返回报文", self.No, interfaceNo)
+        set_excel(self.msg, "预期结果", self.No, interfaceNo)
+
+    def tearDown(self):
+        self.log.build_case_line("请求报文", self.data)
+        self.log.build_case_line("返回报文", self.response)
+        self.log.build_case_line("预期结果", self.msg)
+        self.log.build_end_line(interfaceNo + "--CASE" + self.No)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ interface/userauth/cancelCrowdAuth.py
@@ -0,0 +1,98 @@
+# -*- coding:utf-8 -*-
+
+from utils.baseLog import MyLog
+from utils.baseHttp import ConfigHttp
+from utils.baseUtils import *
+import unittest
+import paramunittest
+from service.gainName import getFullName
+from service.districtcode import gennerator
+interfaceNo = "cancelCrowdAuth"
+name = "取消学会会员认证"
+
+req = ConfigHttp()
+
+
+@paramunittest.parametrized(*get_xls("interfaces.xls", interfaceNo))
+class 取消学会会员认证(unittest.TestCase):
+    def setParameters(self, No, 测试结果, 请求报文, 返回报文, 测试用例, url, realname, idcard, 预期结果):
+        self.No = str(No)
+        self.url = str(url)
+        self.realname = str(realname)
+        self.idcard = str(idcard)
+
+
+    def setUp(self):
+        self.log = MyLog.get_log()
+        self.logger = self.log.logger
+        self.log.build_start_line(interfaceNo + name + "CASE " + self.No)
+        print(interfaceNo + name + "CASE " + self.No)
+
+    """取消学会会员认证"""
+    def test_body(self):
+        req.httpname = "KPTEST"
+        self.url = get_excel("url", self.No, interfaceNo)
+        # 真实的姓名
+        self.realname = get_excel("realname", self.No, interfaceNo)
+        # 身份证号
+        self.idcard = get_excel("idcard", self.No, interfaceNo)
+        # 获取登录sheet页中token
+        self.token = get_excel("token", self.No, "login")
+
+        self.data = {
+            "real_name": self.realname,
+            "id_card": self.idcard,
+            "v": "3.11.0",
+            "system": "5",
+            "device_model": "HUAWEI P10",
+            "system_version": "V1.0.0",
+            "channel": "5"
+        }
+        print(self.data)
+        if self.token == "":
+            self.urlq = self.url
+            self.logger.info(interfaceNo + ">>>>token为空=====" + self.urlq)
+        else:
+            self.urlq = self.url + "&&token=" + self.token
+            self.logger.info(interfaceNo + ">>>>token=====" + self.urlq)
+        req.set_url(self.urlq)
+        req.set_data(self.data)
+        self.response = req.post()
+        print(self.response)
+        try:
+            self.retcode = self.response["code"]
+        except Exception:
+            self.logger.error("报文返回为空！")
+            print("报文返回为空！")
+        self.check_result()
+        self.wr_excel()
+
+    # 检查数据结果
+    def check_result(self):
+        try:
+            self.assertEqual(self.retcode, 0, self.logger.info("是否取消学会会员认证"))
+            set_excel("pass", "测试结果", self.No, interfaceNo)
+            self.logger.info("测试通过")
+        except AssertionError:
+            set_excel("fail", "测试结果", self.No, interfaceNo)
+            self.msg = self.response["msg"]
+            self.logger.error("测试失败")
+        self.msg = self.response["msg"]
+        self.logger.info(self.msg)
+
+    # 写入xls文件中
+    def wr_excel(self):
+        set_excel(self.data, "请求报文", self.No, interfaceNo)
+        set_excel(self.response, "返回报文", self.No, interfaceNo)
+        set_excel(self.msg, "预期结果", self.No, interfaceNo)
+
+    def tearDown(self):
+        self.log.build_case_line("请求报文", self.data)
+        self.log.build_case_line("返回报文", self.response)
+        self.log.build_case_line("预期结果", self.msg)
+        self.log.build_end_line(interfaceNo + "--CASE" + self.No)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
