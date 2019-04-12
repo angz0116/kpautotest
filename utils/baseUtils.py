e22c62b12a1c649252f6c77caefcd717cb672cb7
za_ai@163.com <za_ai@163.com>
2019-04-12 14:47:24 +0800

    2019-04-12 14:48提交

--- utils/baseUtils.py
+++ utils/baseUtils.py
@@ -12,8 +12,8 @@ from xml.etree import ElementTree as ElementTree
 
 
 database = {}
-#proDir = os.path.split(os.getcwd())[0]
-proDir = os.path.abspath(os.path.join(os.getcwd(), "../.."))
+proDir = os.path.split(os.getcwd())[0]
+#proDir = os.path.abspath(os.path.join(os.getcwd(), "../.."))
 print(proDir)
 dataPath = os.path.join(proDir, "config", "interfaces.xls")
 sql_path = os.path.join(proDir, "config", "SQL.xml")
